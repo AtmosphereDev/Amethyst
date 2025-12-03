ca7fcc8726e23583f51fc5dd8eeba51ea561a6c7
Tuna156 <2027156@scprep.org>
2025-12-03 15:47:29 -0500

    Update LevelRendererPlayer::getFov and BaseOptions::getSensitivity to new sig comment format

--- AmethystAPI/src/mc/src-client/common/client/options/BaseOptions.hpp
+++ AmethystAPI/src/mc/src-client/common/client/options/BaseOptions.hpp
@@ -1,6 +1,9 @@
+/// @symbols
 #pragma once
+#include <amethyst/Imports.hpp>
 
 class BaseOptions {
 public:
-    float getSensitivity(unsigned int inputMode);
+	/** @sig {40 53 48 83 EC ? 80 B9 ? ? ? ? ? 8B DA} */
+    MC float getSensitivity(unsigned int inputMode);
 };
