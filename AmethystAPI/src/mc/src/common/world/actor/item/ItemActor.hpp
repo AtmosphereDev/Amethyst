ca295ca30d03372337aef87ffbc6db1e43927445
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-12-08 12:33:11 +0000

    misc

--- AmethystAPI/src/mc/src/common/world/actor/item/ItemActor.hpp
+++ AmethystAPI/src/mc/src/common/world/actor/item/ItemActor.hpp
@@ -5,6 +5,14 @@
 class ItemActor : public Actor {
 public:
 	ItemStack mItem;
+	int32_t mAge;
+	int32_t mPickupDelay;
+	int32_t mThrowTime;
+	float mBobOffs;
+	int32_t mHealth;
+	int32_t mLifeTime;
+	bool mIsInItemFrame;
+	bool mIsFromFishing;
 };
 
 static_assert(offsetof(ItemActor, mItem) == 1096);
